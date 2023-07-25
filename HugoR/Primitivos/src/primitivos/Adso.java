b2d77bd3ae28a50937343f16a42f36380f8111cc
Hugoecom <hugo.ecom@hotmail.com>
2023-07-25 13:43:17 -0500

    Añadida la Carpeta de HugoRincon

--- HugoR/Primitivos/src/primitivos/Adso.java
+++ HugoR/Primitivos/src/primitivos/Adso.java
@@ -1,12 +1,19 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package primitivos;
 
-
+/**
+ *
+ * @author SENA
+ */
 public class Adso {
 
 
     public static void main(String[] args) {
-        
+        // TODO code application logic here
         byte numeroByte = 127;
     
         System.out.println("numeroByte = " + numeroByte);
