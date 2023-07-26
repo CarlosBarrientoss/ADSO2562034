77ffa3551016ee34a122d3d8ac0e8d40badfa50d
DanielaL <karenleon1247@gmail.com>
2023-07-26 14:43:43 -0500

    Clase 2 Poo

---
+++ DanielaL/Poo/src/poo/Poo.java
@@ -0,0 +1,47 @@
+package poo;
+
+import java.util.Scanner;
+
+public class Poo {
+    public static void main(String[] args) {
+        Operadores instanciaOperadores = new Operadores();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa el primer número: ");
+        instanciaOperadores.a = sc.nextInt();
+
+        System.out.println("Ingresa el primer número: ");
+        instanciaOperadores.b = sc.nextInt();
+
+        instanciaOperadores.sumar();
+        instanciaOperadores.resta();
+        instanciaOperadores.multiplicar();
+        instanciaOperadores.dividir();
+
+        instanciaOperadores.asignacionSuma();
+        instanciaOperadores.asignacionResta();
+        instanciaOperadores.asignacionMultiplicar();
+        instanciaOperadores.asignacionDividir();
+        instanciaOperadores.asignacionModulo();
+
+        instanciaOperadores.preIncremento();
+        instanciaOperadores.preDecremento();
+        instanciaOperadores.postIncremento();
+        instanciaOperadores.postDecremento();
+
+        instanciaOperadores.opIgualdadEnteros();
+        instanciaOperadores.opDiferenciaEnteros();
+
+        Scanner palabra = new Scanner(System.in);
+        System.out.println("Ingresa la primer palabra: ");
+        instanciaOperadores.cadenaUno = palabra.nextLine();
+
+        System.out.println("Ingresa la segunda palabra: ");
+        instanciaOperadores.cadenaDos = palabra.nextLine();
+
+        instanciaOperadores.opIgualdadString();
+        instanciaOperadores.opIgualdadEquals();
+
+        instanciaOperadores.ternario();
+    }
+}
