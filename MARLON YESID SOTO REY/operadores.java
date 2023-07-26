e0aaf67ac5546da6b5e94b5ce1a22c769144ca8d
MARLON <masorey199@gmail.com>
2023-07-26 14:39:08 -0500

    agregando archivo marlon

---
+++ MARLON YESID SOTO REY/operadores.java
@@ -0,0 +1,30 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero1 = 50;
+        int numero2 = 30;
+
+        
+        int suma = (numero1 + numero2);
+        System.out.println("Suma: " + suma); 
+
+        
+        int resta = (numero1 - numero2);
+        System.out.println("Resta: " + resta);
+
+        
+        int multiplicacion = (numero1 * numero2);
+        System.out.println("Multiplicación: " + multiplicacion);
+      
+        int division = (numero1 / numero2);
+        System.out.println("División: " + division); 
+    }
+}
+
+    
+    public void operadorDeAsignacionSuma (){
+       int  a+=1;
+       int b+=1;
+ System.out.println("a = " + a);
+ System.out.println("a = " + a);
+}
+
