065c3309551265da1fdfe0a11cf77b1e74c34408
Reyespa <132710926+Reyespa@users.noreply.github.com>
2023-07-26 14:59:58 -0500

    Add files via upload
    clase poo

---
+++ POO.java
@@ -0,0 +1,33 @@
+
+package PooOperadores;
+import java.util.Scanner;
+
+public class POO {
+    public static void main(String[] args) {
+        Scanner recibirNumero = new Scanner (System.in);
+                
+        Operadores instanciaOperadores = new Operadores();
+        
+        System.out.println("Ingrese el numero a: ");
+        instanciaOperadores.a = recibirNumero.nextInt();
+        System.out.println("Ingrese el numero b: ");
+        instanciaOperadores.b = recibirNumero.nextInt();
+        
+        instanciaOperadores.sumar();
+        instanciaOperadores.restar();
+        instanciaOperadores.multiplicar();
+        instanciaOperadores.dividir();
+        instanciaOperadores.Asignacionsuma();
+        instanciaOperadores.Asignacionresta();
+        instanciaOperadores.Asigancionmultiplicar();
+        instanciaOperadores.Asignaciondividir();
+        instanciaOperadores.AsignacionMod();
+        instanciaOperadores.preIncrementoSum();
+        instanciaOperadores.preIncrementoRes();
+        instanciaOperadores.postIncrementoSum();
+        instanciaOperadores.postIncrementoRes();
+        instanciaOperadores.igualdad();
+        instanciaOperadores.ternario();
+        instanciaOperadores.ternarioPar();
+    }
+}
