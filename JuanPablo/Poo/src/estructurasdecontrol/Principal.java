7acfe765d615340a32bb6a5aabd83e3fcb18a954
xXjunior02 <junior.arias04yt@gmail.com>
2023-07-27 14:28:39 -0500

    subiendo archivos

---
+++ JuanPablo/Poo/src/estructurasdecontrol/Principal.java
@@ -0,0 +1,20 @@
+
+package estructurasdecontrol;
+
+public class Principal {
+    public static void main(String[] args){
+        //instanciando la clase condicionales
+        Condicionales imprimirCondicionales=new Condicionales();
+        imprimirCondicionales.condicionIf();
+        imprimirCondicionales.elseIF();
+        imprimirCondicionales.estaciones();
+        imprimirCondicionales.condicionalSwitch();
+        imprimirCondicionales.frutas();
+        imprimirCondicionales.staciones();
+        imprimirCondicionales.condicionalWhile();
+        imprimirCondicionales.doWhile();
+        imprimirCondicionales.login();
+    }
+    
+    
+}
